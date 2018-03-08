52afd306735915eb1cd873d900b02603d9df9f99
Gijs <g.vandenbiggelaar@gmail.com>
2018-03-08 10:27:08 +0100

    Implemented some sort of MVC to the Resources Assignment

---
+++ Assets/Workshops/Resources Workshop/Scripts/Resource.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resource : MonoBehaviour {
+
+    public int amount;
+    public int startingAmount;
+
+    public Text text;
+
+    public void Awake()
+    {
+        amount = startingAmount;
+        UpdateUI();
+    }
+
+    public void AddAmount(int value)
+    {
+        amount += value;
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        text.text = amount.ToString();
+    }
+
+}
