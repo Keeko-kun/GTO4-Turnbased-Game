7e099c854464cc5fc470b3647d47008f4d682f14
Gijs <g.vandenbiggelaar@gmail.com>
2018-03-08 09:28:07 +0100

    Made Resources Assignment

---
+++ Assets/Resources Workshop/ResourcesManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesManager : MonoBehaviour {
+
+    public int gold;
+    public int wood;
+
+    public GameObject goldText;
+    public GameObject woodText;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    public void AddGold(int amount)
+    {
+        gold += amount;
+        UpdateText();
+    }
+
+    public void AddWood(int amount)
+    {
+        wood += amount;
+        UpdateText();
+    }
+
+    public void BuyWeapon()
+    {
+        gold -= 100;
+        wood -= 50;
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        goldText.GetComponent<Text>().text = "Gold: " + gold.ToString();
+        woodText.GetComponent<Text>().text = "Wood: " + wood.ToString();
+    }
+
+}
