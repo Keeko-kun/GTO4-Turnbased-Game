f8ff615ec38b9d192be7d464eadbf6acdfd5c13b
Shi~ <g.vandenbiggelaar@gmail.com>
2018-04-07 11:34:40 +0200

    Added an intro screen

---
+++ Assets/Scripts/AutoFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoFade : MonoBehaviour {
+
+    private fadePanel panel;
+
+	// Use this for initialization
+	void Start () {
+        panel = GetComponent<fadePanel>();
+        StartCoroutine(Fade());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator Fade()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.5f);
+            panel.visible = !panel.visible;
+        }
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    public fadePanel blackness;
+
+    private bool clickedA = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("joystick button 0") && !clickedA)
+        {
+            StartCoroutine(ClickedA());
+        }
+	}
+
+    private IEnumerator ClickedA()
+    {
+        clickedA = true;
+        blackness.visible = true;
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("BasicScene");
+    }
+}
